a3f2a3ac84330130bb9247fd7afb2c5ab9bd2e26
EmoHawk <bobalob123@hotmail.com>
2014-02-25 10:56:59 +0000

    Create UninformedSearch.java

---
+++ UninformedSearch.java
@@ -0,0 +1,43 @@
+import rp13.search.util.ActionStatePair;
+import rp13.search.util.EqualityGoalTest;
+
+public class UninformedSearch<ActionStatePair> {
+	
+	/*private Queue<ActionStatePair> agenda;
+	private EqualityGoalTest goalTest;
+	private ActionStatePair node;
+	//private Stack<ActionStatePair> agenda;
+	
+	public UninformedSearch(ActionT _action, StateT _state){
+		agenda = new Queue<ActionStatePair>();
+		node = new ActionStatePair(_action, _state);
+		//agenda = new Stack<T>();
+		
+	}
+	
+	public ActionStatePair Search()
+	{
+		
+		while(!agenda.isEmpty())
+		{
+			node = agenda.pop();
+			if(goalTest.isGoal(node.getState())
+			{
+				return node;
+			}
+			else
+			{
+				//generate successors
+				// ...
+				for(SearchNode node : successors)
+				{
+					agenda.push(node);
+				}
+			}
+		}	
+	}
+	*/
+}
+/*
+
+*/
